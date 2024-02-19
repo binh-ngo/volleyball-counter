2f647ba54e225449bef51870294521d63deaea29
Binh Ngo <bnngo97@gmail.com>
2024-02-19 10:15:47 -0800

    Initial Commit

---
+++ volleyball-counter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  volleyball-counter
+//
+//  Created by Binh Ngo on 2/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ volleyball-counter/volleyball_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  volleyball_counterApp.swift
+//  volleyball-counter
+//
+//  Created by Binh Ngo on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct volleyball_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
